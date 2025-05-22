12e3fc0e216474b582325a50ea591ec48cae0ce7
Gaston Montiel <gaston.montiel@these.com.uy>
2025-05-21 23:53:33 -0300

    factory

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,17 @@ module EcommerceBase
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.factory_bot suffix: "factory"
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
